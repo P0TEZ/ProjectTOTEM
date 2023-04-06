0560bebb18645debf78f1b423ae5276ac0871232
jairaume <jerome.rascle@student.junia.com>
2023-04-06 13:38:08 +0200

    nestable list start

--- totem-web-app/src/components/TotemList/TotemList.tsx
+++ totem-web-app/src/components/TotemList/TotemList.tsx
@@ -3,7 +3,18 @@ import Nestable from 'react-nestable';
 
 
 export default function TotemList() {
-  return (
-    <Nestable />
-  )
+
+	// get list of totems from the server
+	const [totems, setTotems] = React.useState<any[]>([])
+	React.useEffect(() => {
+		fetch('http://'+process.env.REACT_APP_CENTRAL_ADRESS+':5050/admin/?token='+localStorage.getItem('token'))
+		.then(response => response.json())
+		.then(data => {
+			setTotems(data)
+		})
+	},[])
+
+    return (
+        <Nestable />
+    )
 }
