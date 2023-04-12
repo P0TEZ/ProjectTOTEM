70c88f7099b43799836af20a07f7bf8ef330b19e
jairaume <jerome.rascle@student.junia.com>
2023-04-12 16:05:11 +0200

    fix miscellaneaous things

--- totem-web-app/src/components/Status/Status.tsx
+++ totem-web-app/src/components/Status/Status.tsx
@@ -1,33 +1,34 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-import './Status.scss'
+import "./Status.scss";
 
-export default function Status(props : any) {
-    const [statusClass, setStatusClass] = useState("status connecting");
+export default function Status(props: any) {
+	const [statusClass, setStatusClass] = useState("status connecting");
 
-    useEffect(() => {
-        switch(props.status){
-            case "Connecté":
-                setStatusClass("status connected");
-                break;
-            case "Déconnecté":
-                setStatusClass("status disconnected");
-                break;
-            default:
-                setStatusClass("status connecting");
-                break;
-        }
-    }, [props.status])  
+	useEffect(() => {
+		switch (props.status) {
+			case "Connecté":
+				setStatusClass("status connected");
+				break;
+			case "Déconnecté":
+				setStatusClass("status disconnected");
+				break;
+			default:
+				setStatusClass("status connecting");
+				break;
+		}
+	}, [props.status]);
 
-    return (
-        <div className='statusContainer' data-aos="fade-down" data-aos-delay="200">
-            <h1 className='fs-headline-4'>
-                <span className='fs-headline-6'>N°</span> <strong className='monument'>{props.code}</strong>
-            </h1>
-            <p className='fs-body-1'>
-                <strong className={statusClass}>{props.status}</strong>
-            </p>
-        </div>
-    )
+	return (
+		<div className="statusContainer" data-aos="fade-down" data-aos-delay="200">
+			<h1 className="fs-headline-4">
+				<span className="fs-headline-6">N°</span>{" "}
+				<strong className="monument">{props.code}</strong>
+			</h1>
+			<p className="fs-body-1 bold">
+				<strong className={statusClass}>{props.status}</strong>
+			</p>
+		</div>
+	);
 }
