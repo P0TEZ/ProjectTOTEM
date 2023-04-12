a645f62e6cd921061f8be30a2b5ca9bac66614e6
jairaume <jerome.rascle@student.junia.com>
2023-04-12 10:54:19 +0200

    rework interface

--- totem-web-app/src/index.tsx
+++ totem-web-app/src/index.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import {BrowserRouter} from 'react-router-dom';
-import { AppThemeProvider } from './context/AppTheme';
-import { UserProvider } from './context/User';
+import { BrowserRouter } from "react-router-dom";
+import { AppThemeProvider } from "./context/AppTheme";
+import { UserProvider } from "./context/User";
 
-import 'react-nestable/dist/styles/index.css';  
+import "react-nestable/dist/styles/index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <AppThemeProvider>
-      <UserProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserProvider>
-    </AppThemeProvider>
+	<AppThemeProvider>
+		<UserProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</UserProvider>
+	</AppThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- totem-web-app/src/pages/Interface/Interface.tsx
+++ totem-web-app/src/pages/Interface/Interface.tsx
@@ -1,121 +1,78 @@
-import React from 'react'
-import { useNavigate} from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import './Interface.scss'
-import './KnobOverlay.scss'
+import "./Interface.scss";
 
-import Status from '../../components/Status/Status'
-import KnobPage from '../Knob/Knob'
-import LVRButtons from '../../components/LVRButtons/LVRButtons'
-import Dropdown from '../../components/Dropdown/Dropdown'   
+import Status from "../../components/Status/Status";
+import KnobComponent from "../../components/Knob/Knob";
 
-import { UserContext } from '../../context/User'
+import { UserContext } from "../../context/User";
 
-import {IoHandRight} from 'react-icons/io5'
-import { toast } from 'react-hot-toast'
-import { CSSTransition } from 'react-transition-group'
-
-const texts = {
-    "L": {
-        title: "Intensité gauche",
-        desc: "Force avec laquelle vous ressentez la musique sur le vibreur gauche",
-        param_name: 'intensite_l'
-    },
-    "R": {
-        title: "Intensité droite",
-        desc: "Force avec laquelle vous ressentez la musique sur le vibreur droit",
-        param_name: 'intensite_r'
-    },
-    "V":{
-        title: "Volume",
-        desc: "Volume global",
-        param_name: 'volume'
-    },
-}
+import { IoHandRight } from "react-icons/io5";
+import { toast } from "react-hot-toast";
 
 function Interface(props: any) {
-    const nodeRef = React.useRef(null)
-    const navigate = useNavigate()
-    const [status, setStatus] = useState("Connexion")
-    const [helpAsked, setHelpAsked] = useState(false)
-    const [inProp, setInProp] = useState(false);
-    const [animClass, setAnimClass] = useState("knob-popup-L")
-    const [knob, setKnob] = useState("L")
-    const { userInfo } = React.useContext(UserContext)
+	const navigate = useNavigate();
+	const [status, setStatus] = useState("Connexion");
+	const [helpAsked, setHelpAsked] = useState(false);
+	const { userInfo } = React.useContext(UserContext);
 
-    useEffect(() => {
-        document.title = "TOTEM"
-        // console.log(userInfo)
-        if(userInfo.TotemId === '' || userInfo.token === ''){
-            console.log("Nope !")
-            navigate("/code")
-        }
-        setStatus("Connecté")
-    }, [userInfo, navigate]);
+	useEffect(() => {
+		document.title = "TOTEM";
+		// console.log(userInfo)
+		if (userInfo.TotemId === "" || userInfo.token === "") {
+			console.log("Nope !");
+			navigate("/code");
+		}
+		setStatus("Connecté");
+	}, [userInfo, navigate]);
 
-    // Assistance
-    const handleHelp = () => {
-        setHelpAsked(!helpAsked)
-        console.log(helpAsked)
-    }
+	// Assistance
+	const handleHelp = () => {
+		setHelpAsked(!helpAsked);
+		console.log(helpAsked);
+	};
 
-    useEffect(()=>{
-        if(helpAsked){
-            toast.promise(
-                new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        setHelpAsked(false)
-                        resolve("ok")
-                    }
-                    , 5000)
-                }),
-                {
-                    loading: "Demande d'assistance en cours...",
-                    success: <b>Assistance terminée !</b>,
-                    error: <b>Assistance échouée !</b>,
-                }
-            )
-           
-        }
-    }, [helpAsked])
+	useEffect(() => {
+		if (helpAsked) {
+			toast.promise(
+				new Promise((resolve, reject) => {
+					setTimeout(() => {
+						setHelpAsked(false);
+						resolve("ok");
+					}, 5000);
+				}),
+				{
+					loading: "Demande d'assistance en cours...",
+					success: <b>Assistance terminée !</b>,
+					error: <b>Assistance échouée !</b>,
+				}
+			);
+		}
+	}, [helpAsked]);
 
+	return (
+		<>
+			<div id="InterfacePage" className="PAGE_CONTAINER">
+				<Status code={userInfo.TotemId} status={status} />
 
-    // Knobs
-    const openKnob = (knob:string)=>{
-        // console.log("open knob", knob)
-        setKnob(knob)
-        setInProp(true)
-        setAnimClass("knob-popup-"+knob)
-        // console.log(animClass)
-    }
-    
-    
+				<KnobComponent />
 
-    return (
-        <>
-            <CSSTransition nodeRef={nodeRef} in={inProp} classNames={animClass} unmountOnExit timeout={200}>
-                <div id="KnobContainer" ref={nodeRef}>
-                        <button style={{position:"absolute", bottom:'0', zIndex:1000}} onClick={() => setInProp(false)}>Leave</button>
-                        <KnobPage pos={knob} texts={texts} />
-                </div>
-            </CSSTransition>
-            <div id="InterfacePage" className='PAGE_CONTAINER'>
-
-                <Status code={userInfo.TotemId} status={status}/>
-
-                <LVRButtons openKnob={openKnob}/>
-
-                <Dropdown />
-                
-                <div className="helpBtn" onClick={()=>handleHelp()} data-aos="fade-up" data-aos-delay="800">
-                    <IoHandRight className={`c-primary fs-headline-2 ${helpAsked && "helpAsked"}`}/>
-                    <p className='fs-body-1 c-primary'>Assistance</p>
-                </div>
-                
-            </div>
-        </>
-    )
+				<div
+					className="helpBtn"
+					onClick={() => handleHelp()}
+					data-aos="fade-up"
+					data-aos-delay="800"
+				>
+					<IoHandRight
+						className={`c-primary fs-headline-2 ${helpAsked && "helpAsked"}`}
+					/>
+					<p className="fs-body-1 c-primary">Assistance</p>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default Interface
+export default Interface;
