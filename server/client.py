3e7e553ffb8483a721bf391778da83175bcf137a
FloraCousin <60597446+FloraCousin@users.noreply.github.com>
2023-04-13 14:24:07 +0200

    Add files via upload

--- server/client.py
+++ server/client.py
@@ -4,8 +4,10 @@ import time
 txt = open("number.txt", 'r')
 number = txt.readline()
 
+#UDP_IP = '192.168.43.239'
 UDP_IP = 'localhost'
 UDP_PORT = 6000
+TIME_PING = 20
 
 client = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -13,4 +15,4 @@ client.connect((UDP_IP, UDP_PORT))
 
 while(True):
     client.sendto(str.encode(number), (UDP_IP, UDP_PORT))
-    time.sleep(20)
+    time.sleep(TIME_PING)
--- server/server.py
+++ server/server.py
@@ -3,6 +3,10 @@ import threading
 import time
 import psycopg2
 
+UDP_IP_ADDRESS = "localhost"
+UDP_PORT_NO = 6000
+TIME_PING = 50
+
 ###################################### Connexion BDD
 try:
     conn = psycopg2.connect(
@@ -15,113 +19,85 @@ try:
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 
-UDP_IP_ADDRESS = "localhost"
-UDP_PORT_NO = 6000
-
 ###################################### Ping
 table = []
-tableId = []
 
-def endChrono(table, tableId):
-    requestIdInBDD = """SELECT totem_id FROM totem"""
+def findIdInBDD():
+    requestIdInBDD = """SELECT totem_id, totem_ip FROM totem"""
     try:
         listIdBDD = []
+        listIpBDD = []
         cur = conn.cursor()
         cur.execute(requestIdInBDD)
         row = cur.fetchone()
         while row is not None:
             listIdBDD.append(row[0])
+            listIpBDD.append(row[1])
             row = cur.fetchone()
         cur.close()
-        print(listIdBDD)
-        listToRemove = list(set(listIdBDD) - set(tableId))
-        listToAdd = list(set(tableId) - set(listIdBDD))
-        for i in range(len(listToRemove)):
-            print("A suppr : " + str(listToRemove[i])) 
-            deleteInput(listToRemove[i])
-        for i in range(len(listToAdd)):
-            for j in range(len(table)):
-                if(int(table[j][0]) == listToAdd[i]):
-                    insertInput(table[j][1], listToAdd[i])  
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
+    return listIdBDD, listIpBDD
+
+
+
+def endChrono(table):
+    tableId = []
+    infoBDD = findIdInBDD()
+    listIdBDD = infoBDD[0]
+    listIpBDD = infoBDD[1]
+    for i in range(len(table)):
+        tableId.append(int(table[i][0]))
+    print("1 : " + str(listIdBDD) + " " + str(tableId))
+    listToRemove = list(set(listIdBDD) - set(tableId))
+    print("to remove : " + str(listToRemove))
+
+    for i in range(len(listToRemove)):
+        for j in range(len(listIdBDD)):
+            if(listIdBDD[j] == listToRemove[i]):
+                print(("truc qui va être supprimé : " + str(int(listIdBDD[j]))) + " " + str(listIpBDD[j]))
+                deleteInput(int(listIdBDD[j]), str(listIpBDD[j]))
+                print("Tnput deleted")
+
+
     
-def timing(table, tableId, time_ping):
-    time.sleep(time_ping)
-    endChrono(table, tableId)
+def timing(table):
+    time.sleep(TIME_PING)
+    endChrono(table)
 
 def ping():
     global table
     while(True):
-        addr = socket.recvfrom(1024)     
-        table.append(addr)
-        print(table)
+        addr = socket.recvfrom(1024)   
+        if(addr):
+            print("ping ! " + str(addr[0]))
+        if(addr not in table):
+            print("add : " + str(addr))  
+            table.append(addr)
+            print("a: " + str(findIdInBDD()[0]))
+            if(int(addr[0]) not in findIdInBDD()[0]):
+                insertInput(int(addr[0]), str(addr[1]))  
 
 def checkPing():
     global table
-    global tableId
-    time_ping = 20
-    tPingTime = threading.Thread(target=timing, args=(table, tableId, time_ping))
+    tPingTime = threading.Thread(target=timing, args=(table,))
     tPingTime.start()
     tPing = threading.Thread(target=ping)
     tPing.start()
     while(True):
         if(tPingTime.is_alive() == False):
-            for i in range(len(table)):
-                id = table[i][0]
-                tableId.append(int(id))
-            print(tableId)
-            tPingTime = threading.Thread(target=timing, args=(table, tableId, time_ping))
+            tPingTime = threading.Thread(target=timing, args=(table,))
             tPingTime.start()
             table = []
-            tableId = []
             print("New Cycle")
-         
-
 
 ###################################### Requêtes BDD
-def addGroupe():
-    groupeIdMax = 0
-    request = """SELECT MAX(groupe_id) FROM groupe;"""
-    try:
-        cur = conn.cursor()
-        cur.execute(request)
-        groupeIdMax = cur.fetchone()[0] + 1
-        conn.commit()
-        cur.close()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    return groupeIdMax
-    
-def delUselessGroupes(groupe_id):
-    request = """SELECT COUNT(groupe_id) FROM totem WHERE groupe_id =(%s);"""
-    try:
-        cur = conn.cursor()
-        cur.execute(request, (groupe_id,))
-        nbGroupe = cur.fetchone()[0]
-        if(nbGroupe == 0):
-            request2 = """DELETE FROM groupe WHERE groupe_id = (%s);"""
-            cur.execute(request2, (groupe_id,))
-            print("suppr!!!!")
-            conn.commit()
-        else:
-            print(nbGroupe)
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-
-def insertInput(address, totemId):
-    request1 = """INSERT INTO public.groupe values (%s);"""
-    request2 = """INSERT INTO public.totem values (%s, %s, %s);"""
+def insertInput(totemId, totemIp):
+    request = """SELECT create_new_totem(%s, %s);"""
     try:
         # create a new cursor
         cur = conn.cursor()
-        # execute the INSERT statement
-        newGroupe = addGroupe()
-        cur.execute(request1, (newGroupe,))
-        cur.execute(request2, (address, totemId, newGroupe))
+        cur.execute(request, (totemId, totemIp))
         # commit the changes to the database
         conn.commit()
         # close communication with the database
@@ -131,25 +107,22 @@ def insertInput(address, totemId):
 
 
 
-def deleteInput(totemId):
-    request = """SELECT groupe_id FROM totem WHERE TOTEM_ID = (%s)"""
-    request2 = """DELETE FROM TOTEM WHERE TOTEM_ID = (%s);"""
+def deleteInput(totemId, totemIp):
+    request = """SELECT delete_totem(%s, %s)"""
     try:
         # create a new cursor
         cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(request, (totemId,))
-        groupe_id = cur.fetchone()[0]
-        print("groupe_id = " + str(groupe_id))
-        cur.execute(request2, (totemId,))
+        cur.execute(request, (totemId, totemIp))
         # commit the changes to the database
         conn.commit()
         # close communication with the database
         cur.close()
-        delUselessGroupes(groupe_id)
+        #delUselessGroupes(groupe_id)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
+
+###################################### Connexion UDP
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 
