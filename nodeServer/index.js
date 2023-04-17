0789c99f36550fc114825a20abbea0c634b29d64
POTEZ <60687263+P0TEZ@users.noreply.github.com>
2023-04-17 09:28:05 +0200

    readme du socket serveur

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,5 +1,11 @@
 const io = require('socket.io')();
 
+/* This code sets up a socket.io server that listens for client connections. When a client connects, it
+logs a message to the console. When a client disconnects, it logs another message to the console.
+When a client emits an 'updated' event, it logs a message to the console indicating that the event
+was received from the client, broadcasts the event to all other connected clients (excluding the
+sender), and logs a message indicating that the broadcast was sent. */
+
 io.on('connection', (client) => {
 
     console.log('Client connected...');
@@ -22,6 +28,9 @@ io.on('connection', (client) => {
 
 });
 
+/* This code is setting up the socket.io server to listen on port 4000 for incoming client connections.
+Once the server is listening on the specified port, it logs a message to the console indicating that
+it is listening on that port. */
 const PORT = 4000
 io.listen(PORT);
 console.log('listening on port ', PORT);
